ee12d20a7c01abe17a3e99013a93d5ceb46b3fbd
xinggsf <xinggsf@gmail.com>
2018-07-27 12:38:14 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- Clean.bat
+++ Clean.bat
@@ -1,4 +1,6 @@
 ﻿:: rd/s/q "Media Cache"2>NUL
+del /s/q *.tmp
+del /s/q *.bak
 ::取得当前批处理所在路径
 cd /d %~dp0
 cd Default
@@ -9,14 +11,3 @@ del /s/f/q "Media Cache\*"
 del /s/f/q "Local Storage\http*"
 del /s/f/q Cache\*
 del /s/f/q GPUCache\*
-del /q bookmarks_*
-del /s/q *.tmp
-del /s/q *.bak
-cd ..
-::设置备份存放的路径以及压缩包文件名
-set ArchiveName=..\dataCent_%date:~5,2%%date:~8,2%.7z
-::要备份扩展，删除下一行 -x!之后的（包括-x!）
-7z.exe u %ArchiveName% -up1q3r2x2y2z2w2 "Local State" Default -x!"Default\Extensions"
-@echo 备份已完成！
-echo.&echo.
-::pause
---
+++ bakup.bat
@@ -0,0 +1,7 @@
+﻿::设置备份存放的路径以及压缩包文件名
+set ArchiveName=..\dataCent_%date:~5,2%%date:~8,2%.7z
+::要备份扩展，删除下一行 -x!之后的（包括-x!）-x!"Default\Extensions"
+7z.exe u %ArchiveName% -up1q3r2x2y2z2w2 "Local State" "Default\Extension*" "Default\* Extension Settings" "Default\Bookmarks" "Default\Preferences" "Default\Shortcuts" "Default\Top Sites"
+@echo 备份已完成！
+echo.&echo.
+::pause
