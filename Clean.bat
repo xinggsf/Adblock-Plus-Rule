c5fbc1a672888dc2f8591994ed3e2a6c9cc3cc7a
xinggsf <xinggsf@gmail.com>
2023-01-28 21:30:13 +0800

    更新 Clean.bat

--- Clean.bat
+++ Clean.bat
@@ -1,20 +1,21 @@
-﻿::chrome核浏览器垃圾清理批处理，放在userData目录
+﻿:: chrome核浏览器垃圾清理批处理，放在userData目录
 :: rd/s/q "Media Cache"2>NUL
 del /s/q *.tmp
 del /s/q *.bak
 del /s/f/q "Crashpad\reports\*"
 del /s/f/q "GrShaderCache\GPUCache\*"
 del /s/f/q "Guest Profile\*"
-::取得当前批处理所在路径
-::cd /d %~dp0
+:: 取得当前批处理所在路径
+:: cd /d %~dp0
 cd Default
 del /f/q ChromeDWriteFontCache
 del /s/f/q "Pepper Data\*"
 del /s/f/q "Application Cache\*"
 del /s/f/q "Media Cache\*"
-for /d %%d in (IndexedDB\http*) do rd/s/q %%d
+del /s/f/q "Service Worker\CacheStorage\*"
+for /d %%d in ("Service Worker\CacheStorage\*") do rd /s/q "%%d"
+for /d %%d in ("Local Storage\http*") do rd /s/q "%%d"
+for /d %%d in ("IndexedDB\http*") do rd /s/q "%%d"
 del /s/f/q Cache\*
 del /s/f/q GPUCache\*
-cd "Local Storage"
-for /d %%d in (http*) do rd/s/q %%d
 ::pause
